946ba857be7116deb8719c998ddf83d8f8d3e22f
Jimmy Ray <jimmyraywv@yahoo.com>
2013-12-10 14:00:03 -0500

    Added Object Rocket REST API client place holder.

---
+++ src/main/java/org/jimmyray/mongo/rest/client/ObjectRocketClient.java
@@ -0,0 +1,58 @@
+package org.jimmyray.mongo.rest.client;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import com.sun.jersey.api.client.Client;
+import com.sun.jersey.api.client.ClientResponse;
+import com.sun.jersey.api.client.WebResource;
+
+public class ObjectRocketClient {
+	private static Logger log = LoggerFactory.getLogger(ObjectRocketClient.class);
+
+	private String apiKey;
+
+	public String getApiKey() {
+		return apiKey;
+	}
+
+	public void setApiKey(String apiKey) {
+		this.apiKey = apiKey;
+	}
+
+	public static void main(String[] args) {
+
+	}
+
+	public String listDatabases() {
+		String output = null;
+
+		try {
+
+			Client client = Client.create();
+
+			
+			//TODO Flesh out the URI
+			WebResource webResource = client.resource("<URI>"
+					+ this.apiKey);
+
+			ClientResponse response = webResource.accept("application/json")
+					.get(ClientResponse.class);
+
+			if (response.getStatus() != 200) {
+				throw new RuntimeException("REST API Failure: "
+						+ response.getEntity(String.class)
+						+ ", HTTP error code : " + response.getStatus());
+			}
+
+			output = response.getEntity(String.class);
+
+		} catch (Exception e) {
+			log.error(e.getLocalizedMessage());
+			output = e.getLocalizedMessage();
+		}
+
+		return output;
+
+	}
+}
